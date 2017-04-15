222a1d67bb2ff551f8dacfe244b683d35c7df54a
Ben Rodenhäuser <ben@karo.berlin>
2017-04-16 00:06:21 +0200

    continue with section easy 7

--- 07_Easy_07/staggered_caps_part1.rb
+++ 07_Easy_07/staggered_caps_part1.rb
@@ -1,13 +1,10 @@
-=begin
+def staggered_case(string, parity: 'even')
+  choice = (parity == 'even' ? 0 : 1)
 
-Write a method that takes a String as an argument, and returns a new String that
-contains the original value using a staggered capitalization scheme in which every
-other character is capitalized, and the remaining characters are lowercase.
-Characters that are not letters should not be changed, but count as characters
-when switching between upper and lowercase.
+  string.downcase.split('').map.with_index do |char, index|
+    index % 2 == choice ? char.upcase : char
+  end.join
+end
 
-=end
-
-staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-staggered_case('ALL_CAPS') == 'AlL_CaPs'
-staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('I Love Launch School!', parity: 'odd') == 'i lOvE LaUnCh sChOoL!'
