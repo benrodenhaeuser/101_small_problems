7a92ecfa4831553133ce4f47034a12634cda3b69
Ben Rodenhäuser <ben@karo.berlin>
2017-07-18 18:50:20 +0200

    minor

---
+++ code_wars/skyscrapers.rb
@@ -0,0 +1,12 @@
+
+def consistent?(array, left_clue, right_clue)
+  # return true if array is consistent with the two clues
+end
+
+def visible_left(array)
+  # return number of buildings visible from left given array
+end
+
+def visible_right(array)
+  # return number of buildings visible from right given array
+end
