9d20781817fb7961e1718b4436013803055aad29
Ben Rodenhäuser <ben@karo.berlin>
2017-11-03 21:36:02 +0100

    submit solution to crypto-square

--- exercism/ruby/crypto-square/crypto_square.rb
+++ exercism/ruby/crypto-square/crypto_square.rb
@@ -1,2 +1,39 @@
-# organize the text in a matrix
-# the columns of the matrix are the words of the cyphertext
+module BookKeeping
+  VERSION = 1
+end
+
+class Crypto
+  def initialize(plaintext)
+    @plaintext = plaintext.gsub(/[^A-Z0-9]/i, '').downcase
+  end
+
+  def ciphertext
+    return @plaintext if @plaintext.empty?
+
+    matrix
+      .transpose
+      .map { |row| row.join }
+      .join(' ')
+  end
+
+  private
+
+  def matrix
+    plain_with_blanks = @plaintext + (' ' * blanks)
+
+    plain_with_blanks
+      .chars
+      .each_slice(cols)
+      .with_object([]) do |slice, matrix|
+        matrix << slice
+      end
+  end
+
+  def cols
+    Math.sqrt(@plaintext.length).ceil.to_i
+  end
+
+  def blanks
+    (cols - (@plaintext.length % cols)) % cols
+  end
+end
