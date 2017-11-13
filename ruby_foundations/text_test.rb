cbb3196cc4d1c7c86d171ff588d6932528373597
Ben Rodenhäuser <ben@karo.berlin>
2017-11-14 00:41:08 +0100

    more exercises

---
+++ ruby_foundations/text_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative 'text'
+
+class TextTest < Minitest::Test
+  def setup
+    # read the sample test from file
+    # convert sample test
+  end
+
+  def test_swap
+
+  end
+
+  def teardown
+    #
+  end
+
+end
