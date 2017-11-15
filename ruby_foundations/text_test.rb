1f1e8a350b5bbba9f5653da8c0264b4dbff175e4
Ben Rodenhäuser <ben@karo.berlin>
2017-11-15 22:08:18 +0100

    finish 130 exercises

--- ruby_foundations/text_test.rb
+++ ruby_foundations/text_test.rb
@@ -3,16 +3,32 @@ require_relative 'text'
 
 class TextTest < Minitest::Test
   def setup
-    # read the sample test from file 'sample_text.txt'
-    # convert sample test
+    @file = File.open('sample_text.txt')
+    @sentences = @file.read
+    @text = Text.new(@sentences)
   end
 
   def test_swap
+    actual = @text.swap('a', 'e')
+    expected = <<~HEREDOC
+      Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
+      Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
+      quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo
+      nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum
+      dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
+      et verius meuris, et pheretre mi.
+      HEREDOC
+    assert_equal(expected, actual)
+  end
 
+  def test_word_count
+    expected = 72
+    actual = @text.word_count
+    assert_equal(expected, actual)
   end
 
   def teardown
-    #
+    @file.close
   end
 
 end
