08457929cab1328505a2b1c16448729ded40fb77
Ben Rodenhäuser <ben@karo.berlin>
2017-11-15 18:19:07 +0100

    improved regex

--- exercism/ruby/acronym/acronym.rb
+++ exercism/ruby/acronym/acronym.rb
@@ -4,6 +4,8 @@ end
 
 class Acronym
   def self.abbreviate(phrase)
-    phrase.scan(/[A-Z]+/i).map { |word| word[0].upcase }.join
+    phrase.scan(/\b\w/).map { |word| word[0].upcase }.join
+    # ^ why does this pass?
+    # phrase.scan(/[A-Z]+/i).map { |word| word[0].upcase }.join
   end
 end
