fe697efcbf5f3385bfc0c0eb7518b2cfdba13449
Ben Rodenhäuser <ben@karo.berlin>
2017-11-15 19:54:31 +0100

    finish exercise set 4

--- ruby_foundations/0409.rb
+++ ruby_foundations/0409.rb
@@ -1,1 +1,28 @@
-# Internal vs external iterators / Enumerator
+# define enumerator that yields factorials
+factorials = Enumerator.new do |yielder|
+  number = 0
+  factorial = 1
+
+  loop do
+    yielder << factorial
+    number += 1
+    factorial = factorial * (number)
+  end
+end
+
+# "external" iteration
+def print7(enumerator)
+  index = 0
+  while index < 7
+    p enumerator.next
+    index += 1
+  end
+end
+
+print7(factorials)
+
+# resetting the enumerator
+factorials.rewind
+
+# "internal" iteration
+puts factorials.take(7)
