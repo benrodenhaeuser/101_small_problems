ed2ceca6652a3ce91564acd51ada76fbffa9021f
Ben Rodenhäuser <ben@karo.berlin>
2017-11-18 14:02:12 +0100

    refactor minesweeper

--- exercism/ruby/minesweeper/minesweeper.rb
+++ exercism/ruby/minesweeper/minesweeper.rb
@@ -1,53 +1,39 @@
 class ValueError < ArgumentError; end
 
 class Board
+  MINE = '*'.freeze
+  LAND = ' '.freeze
+  BORDER = '|'.freeze
+
   def self.transform(input)
     new(input).transform
   end
 
-  attr_reader :input
-
   def initialize(input)
     @input = input
-    @width = rows[0].length
-    @height = rows.length
+    @width = @input[0].length
+    @height = height = @input.length
 
-    raise ValueError unless valid?
+    raise ValueError unless valid_input?
   end
 
   def transform
     index_pairs
-      .select { |pair| self[*pair].marker != '*' && mines(*pair) > 0 }
+      .select { |pair| self[*pair].marker == LAND && mines(*pair) > 0 }
       .each { |pair| self[*pair].marker = mines(*pair) }
 
-    wrap(
-      rows.map do |row|
-        wrap(row.map { |square| square.marker }.join, '|')
-      end,
-      [input.first]
-    )
+    to_output
   end
 
   private
 
-  def valid?
-    input.slice(1...-1).all? do |row|
-      row.length == @width + 2 && row[0] == row[-1] && row[0] == '|' &&
-        row.slice(1...-1).chars.all? { |char| [' ', '*'].include?(char) }
-    end
+  def grid
+    @grid ||= @input
+              .map(&:chars)
+              .map { |row| row.map { |marker| Square.new(marker) } }
   end
 
-  def rows
-    @rows ||= input
-      .slice(1...-1)
-      .map(&:chars)
-      .map { |row| row.slice(1...-1) }
-      .map { |row| row.map { |marker| Square.new(marker) } }
-  end
-
-  def squares
-    @squares ||= rows.flatten
-  end
+  Square = Struct.new(:marker)
 
   def index_pairs
     @index_pairs ||= (0...@width).to_a.product((0...@height).to_a)
@@ -56,24 +42,30 @@ class Board
   def [](x, y)
     return nil if x < 0 || x >= @width
     return nil if y < 0 || y >= @height
-    squares[x + y * @width]
+    grid[y][x]
   end
 
   def mines(x, y)
     neighbors(x, y)
-      .select { |neighbor| self[*neighbor].marker == '*' }
+      .select { |neighbor| self[*neighbor].marker == MINE }
       .count
   end
 
   def neighbors(x, y)
-    (x - 1..x + 1).to_a
-      .product((y - 1..y + 1).to_a)
-      .select { |pair| self[*pair] }
+    (x - 1..x + 1).to_a.product((y - 1..y + 1).to_a)
   end
 
-  Square = Struct.new(:marker)
+  def to_output
+    grid.map { |row| row.map(&:marker).join }
+  end
 
-  def wrap(wrapped, wrapper)
-    wrapper + wrapped + wrapper
+  def valid_input?
+    @input[1...-1].all? do |row|
+      row.length == @width &&
+        row[0] == BORDER && row[-1] == BORDER &&
+        row[1...-1].chars.all? do |char|
+          [' ', '*'].include?(char)
+        end
+    end
   end
 end
