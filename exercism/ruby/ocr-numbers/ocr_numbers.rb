c730259fe78e8ea84a28e97bce94e53addd1cf30
Ben Rodenhäuser <ben@karo.berlin>
2017-11-19 22:20:53 +0100

    refactor ocr_numbers

--- exercism/ruby/ocr-numbers/ocr_numbers.rb
+++ exercism/ruby/ocr-numbers/ocr_numbers.rb
@@ -1,4 +1,7 @@
 class OcrNumbers
+  DIGIT_HEIGHT = 4
+  DIGIT_WIDTH = 3
+
   def self.convert(text)
     new(text).convert
   end
@@ -9,82 +12,66 @@ class OcrNumbers
   end
 
   def convert
-    numbers
-      .map(&:convert)
+    @text
+      .split("\n")
+      .each_slice(DIGIT_HEIGHT)
+      .map { |number| OcrNumber.new(number).convert }
       .join(',')
   end
 
-  def numbers
-    @text
-      .split("\n")
-      .each_slice(4)
-      .map do |number|
-        OcrNumber.new(number)
+  def valid_text?
+    ((@text.scan(/\n/).count + 1) % DIGIT_HEIGHT).zero? &&
+      @text.split("\n").all? do |segment|
+        (segment.length % DIGIT_WIDTH).zero?
       end
   end
 
-  def valid_text?
-    ((@text.scan(/\n/).count + 1) % 4).zero? &&
-      @text.split("\n").all? { |segment| (segment.length % 3).zero? }
-  end
-end
+  class OcrNumber
+    DICTIONARY =
+      Hash.new { |hash, key| hash[key] = '?' }.merge!(
+        " _ \n| |\n|_|\n   " => '0',
+        "   \n  |\n  |\n   " => '1',
+        " _ \n _|\n|_ \n   " => '2',
+        " _ \n _|\n _|\n   " => '3',
+        "   \n|_|\n  |\n   " => '4',
+        " _ \n|_ \n _|\n   " => '5',
+        " _ \n|_ \n|_|\n   " => '6',
+        " _ \n  |\n  |\n   " => '7',
+        " _ \n|_|\n|_|\n   " => '8',
+        " _ \n|_|\n _|\n   " => '9'
+      )
 
-class OcrNumber
-  MATCHER = Hash.new { |hash, key| hash[key] = '?' }.merge!(
-    " _ \n| |\n|_|\n   " => '0',
-    "   \n  |\n  |\n   " => '1',
-    " _ \n _|\n|_ \n   " => '2',
-    " _ \n _|\n _|\n   " => '3',
-    "   \n|_|\n  |\n   " => '4',
-    " _ \n|_ \n _|\n   " => '5',
-    " _ \n|_ \n|_|\n   " => '6',
-    " _ \n  |\n  |\n   " => '7',
-    " _ \n|_|\n|_|\n   " => '8',
-    " _ \n|_|\n _|\n   " => '9'
-  )
+    def initialize(number)
+      @number = number
+      @number_of_digits = @number[0].length / DIGIT_WIDTH
+    end
 
-  def initialize(number)
-    @number = number
-  end
+    def convert
+      (0...@number_of_digits)
+        .map { |n| DICTIONARY[digit_at(n)] }
+        .join
+    end
 
-  def convert
-    digits
-      .map { |digit| MATCHER[digit] }
-      .join
-  end
+    def [](address)
+      x, y = address
+      @number[y][x]
+    end
 
-  def digits
-    (0...number_of_digits).map do |n|
-      digit_at(n)
+    def digit_at(n)
+      addresses_for_digit_at(n)
+        .map { |address| self[address] }
+        .each_slice(DIGIT_WIDTH)
+        .map(&:join)
+        .join("\n")
+    end
+
+    def addresses_for_digit_at(n)
+      (0...DIGIT_HEIGHT)
+        .to_a
+        .product((DIGIT_WIDTH * n...DIGIT_WIDTH * (n + 1)).to_a)
+        .map(&:reverse)
     end
   end
 
-  def number_of_digits
-    @number[0].length / 3
-  end
-
-  def digit_at(n)
-    addresses_by_row
-      .select { |address| (n * 3..(n * 3 + 2)).cover?(address.first) }
-      .map { |address| self[address] }
-      .each_slice(3)
-      .map(&:join)
-      .join("\n")
-  end
-
-  def addresses_by_row
-    (0...@number.length)
-      .to_a
-      .product((0...@number[0].length).to_a)
-      .map(&:reverse)
-  end
-
-  def [](address)
-    x, y = address
-    @number[y][x]
-  end
-end
-
-module BookKeeping
-  VERSION = 1
+  private_constant :OcrNumber
 end
--- exercism/ruby/ocr-numbers/ocr_numbers_test.rb
+++ exercism/ruby/ocr-numbers/ocr_numbers_test.rb
@@ -111,7 +111,7 @@ class OcrNumbersTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    # skip
+    skip
     assert_equal 1, BookKeeping::VERSION
   end
 end
