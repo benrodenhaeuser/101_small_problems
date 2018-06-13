b93e44a3f38eb441c4cb53b94f790e6e1a418cc1
Ben Rodenhäuser <ben@karo.berlin>
2018-06-13 07:38:21 +0200

    add javascript exercises

---
+++ exercism/javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,26 @@
+function DnaTranscriber() {
+  this.converter = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U',
+  };
+}
+
+DnaTranscriber.prototype.toRna = function toRna(strand) {
+  var transcribed = '';
+  var i;
+
+  for (i = 0; i < strand.length; i += 1) {
+    if (!this.valid(strand[i])) { throw new Error('Invalid input'); }
+    transcribed += this.converter[strand[i]];
+  }
+
+  return transcribed;
+};
+
+DnaTranscriber.prototype.valid = function valid(segment) {
+  return this.converter[segment];
+};
+
+module.exports = DnaTranscriber;
