91ede040e6c0b4e2471445f33daf3a4b10b2d763
Ben Rodenhäuser <ben@karo.berlin>
2018-06-15 12:17:38 +0200

    more exercism

---
+++ exercism/javascript/matrix/matrix.js
@@ -0,0 +1,29 @@
+function Matrix(str) {
+  this.rows = str
+    .split('\n')
+    .map(rowStr => rowStr
+      .split(' ')
+      .map(numStr => Number(numStr)));
+
+  this.columns = this.transpose();
+}
+
+Matrix.prototype.transpose = function transpose() {
+  const numberOfColumns = this.rows[0].length;
+  const numberOfRows = this.rows.length;
+  const columns = [];
+
+  for (let i = 0; i < numberOfColumns; i += 1) {
+    const currentColumn = [];
+
+    for (let j = 0; j < numberOfRows; j += 1) {
+      currentColumn.push(this.rows[j][i]);
+    }
+
+    columns.push(currentColumn);
+  }
+
+  return columns;
+};
+
+module.exports = Matrix;
